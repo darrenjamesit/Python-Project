2e24cba4842818e4715a323ce36291659fa48c12
Darren James <darrenjames.it@gmail.com>
2023-03-22 18:36:24 +0200

    first items from category 'accessories' added to products

--- database/init.sql
+++ database/init.sql
@@ -31,3 +31,13 @@ create table if not exists images(
     foreign key (prod_id)
         references products(id)
 );
+
+insert into products (name, price, stock, category_id)
+    values
+    ('Aux to Analogue Cable 1.5m', '29.99', '53', '4'),
+    ('3.5mm Aux Cord 3m', '14.99', '44', '4'),
+    ('10m High-Speed Ethernet Cable', '19.99', '29', '4'),
+    ('4K Gold-Plated HDMI Cable 5m', '49.99', '10', '4'),
+    ('3.5mm Aux Splitter 0,5m', '17.99', '32', '4'),
+    ('USB-A to Type-C Cable (Fast Charge) 3m', '59.99', '88', '4'),
+    ('VGA-to-VGA Cable 1,5m', '14.99', '7', '4');
