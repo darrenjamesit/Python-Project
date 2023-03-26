33ee1c29fe3f22d0c0b87a2e8674abcdd491456d
Darren James <darrenjames.it@gmail.com>
2023-03-26 20:09:30 +0300

    updates stock on page (visual only)

---
+++ static/scripts/basket.js
@@ -0,0 +1,9 @@
+const basketButton = document.getElementById("basket");
+    const stockDiv = document.getElementById("stock");
+
+    basketButton.addEventListener("click", () => {
+        const quantity = parseInt(document.getElementById("quantity").value);
+        const currentStock = parseInt(stockDiv.innerText.split(": ")[1]);
+        const newStock = currentStock - quantity;
+        stockDiv.innerText = "Stock: " + newStock;
+    });
