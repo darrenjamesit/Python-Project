12b0565245861b2278121c0531e8beb33c82fe3e
Darren James <darrenjames.it@gmail.com>
2023-03-29 17:29:02 +0300

    fixed the problem with image toggling

--- static/scripts/image.js
+++ static/scripts/image.js
@@ -2,12 +2,15 @@ var currentImg = 0;
 var images = document.getElementById("image-container").getElementsByTagName("img");
 
 function toggleImages() {
+    console.log(currentImg)
     // hide the currently displayed image
     images[currentImg].style.display = "none";
+    images[currentImg].style.visibility = "hidden";
 
     // update the index of the next image to be displayed
     currentImg = (currentImg + 1) % images.length;
 
     // show the next image in sequence
     images[currentImg].style.display = "block";
+    images[currentImg].style.visibility = "visible";
 }
