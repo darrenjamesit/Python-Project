b0c60878b47e36f3efb2303e256b0de6b20cdf17
Darren James <darrenjames.it@gmail.com>
2023-04-09 23:57:13 +0300

    deleted empty lines, and removed alerts (previous message was supposed to refer to total.js

--- static/scripts/basket.js
+++ static/scripts/basket.js
@@ -5,6 +5,4 @@ basketButton.addEventListener("click", () => {
   const quantity = parseInt(document.getElementById("quantity").value);
   const currentStock = parseInt(stockDiv.innerText.split(": ")[1]);
   const newStock = currentStock - quantity;
-
-
 });
