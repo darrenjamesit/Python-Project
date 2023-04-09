57518043d1e52ad641979477b3c86673e4d6eff6
Darren James <darrenjames.it@gmail.com>
2023-04-09 23:55:52 +0300

    created a script to calculate new price for increases and decreases in no. of items in basket

--- static/scripts/basket.js
+++ static/scripts/basket.js
@@ -6,12 +6,5 @@ basketButton.addEventListener("click", () => {
   const currentStock = parseInt(stockDiv.innerText.split(": ")[1]);
   const newStock = currentStock - quantity;
 
-  if (quantity > 0 && newStock >= 0) {
-    stockDiv.innerText = "Stock: " + newStock;
-    alert("Item added to basket!");
-  } else if (quantity <= 0) {
-    alert("Please enter a valid quantity!");
-  } else {
-    alert("Not enough stock available!");
-  }
+
 });
