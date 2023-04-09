73b5ce5dff1d0bc033d72615530fe2dd311012bb
Darren James <darrenjames.it@gmail.com>
2023-04-09 21:15:08 +0300

    fixed bug allowing manual input of more than available stock

--- static/scripts/basket.js
+++ static/scripts/basket.js
@@ -1,10 +1,15 @@
 const basketButton = document.getElementById("basket");
-    const stockDiv = document.getElementById("stock");
+const stockDiv = document.getElementById("stock");
 
-    basketButton.addEventListener("click", () => {
-        const quantity = parseInt(document.getElementById("quantity").value);
-        const currentStock = parseInt(stockDiv.innerText.split(": ")[1]);
-        const newStock = currentStock - quantity;
-        stockDiv.innerText = "Stock: " + newStock;
-        alert("Item added to basket!")
-    });
+basketButton.addEventListener("click", () => {
+  const quantity = parseInt(document.getElementById("quantity").value);
+  const currentStock = parseInt(stockDiv.innerText.split(": ")[1]);
+  const newStock = currentStock - quantity;
+
+  if (newStock >= 0) {
+    stockDiv.innerText = "Stock: " + newStock;
+    alert("Item added to basket!");
+  } else {
+    alert("Not enough stock available!");
+  }
+});
